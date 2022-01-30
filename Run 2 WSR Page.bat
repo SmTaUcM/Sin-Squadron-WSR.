8995b3f975ca5a763f078ac70f2a0ca9133ef76f
Stu Macintosh <SmTaUcM@Yahoo.co.uk>
2022-01-30 11:10:06 +0000

    Report #30

--- Run 2 WSR Page.bat
+++ Run 2 WSR Page.bat
@@ -1,1 +1,1 @@
-start "" http://localhost:3000/reports/30
+start "" http://localhost:3000/reports/31
--- pages/reports/31.js
+++ pages/reports/31.js
@@ -16,10 +16,10 @@ import Link from '../../components/link';
 import config from '../../config';
 import loadActivityData from '../../src/loadSquadronActivityData';
 
-const reportNumber = 30;
-const startDate = '2022-01-23';
-const endDate = '2022-01-29';
-const submissionDate = '2022-01-30';
+const reportNumber = 31;
+const startDate = '2022-01-30';
+const endDate = '2022-02-05';
+const submissionDate = '2022-02-06';
 
 const citations = [
   'XvT-TC 14',
@@ -27,7 +27,7 @@ const citations = [
   'XWA-TC 1',
   'XWA-TC 2', 
 ];
-const citationsChange ='+0';
+const citationsChange ='_____TODO_____';
 
 // These don't seem to change anything. Use: citations.js 
 const orders = [
@@ -122,15 +122,15 @@ const ACTIVITY = {
   // SkyShadow
   6958: {
     communication: 'Discord',
-    flightActivity: 'Chalquilla Cup 2 Practice 1, Squadrons PvP, Battlefront PvP, Star Conflict PvE',
-    otherActivity: 'IOA Duties, CMDR+ Meeting',
+    flightActivity: '',
+    otherActivity: 'IOA Duties',
     notes: '',
   },
   
   // Kazraran
   56002: {
-    communication: 'Discord',
-    flightActivity: 'Star Conflict PvE',
+    communication: '',
+    flightActivity: '',
     otherActivity: '',
     notes: '',
   },
@@ -145,15 +145,15 @@ const ACTIVITY = {
   
   // Robert Hogan
   55859: {
-    communication: 'Discord',
-    flightActivity: 'Chalquilla Cup 2 Practice 1',
+    communication: '',
+    flightActivity: '',
     otherActivity: '',
-    notes: 'Thanks for attending practice!',
+    notes: '',
   },
 
   // Kane Polybius
   56085: {
-    communication: 'Discord',
+    communication: '',
     flightActivity: '',
     otherActivity: '',
     notes: '',
@@ -170,9 +170,9 @@ const ACTIVITY = {
  // Wreckage
   55962: {
     communication: 'Discord',
-    flightActivity: 'Chalquilla Cup 2 Practice 1, Single Player, Squadrons PvP, Star Conflict PvE',
+    flightActivity: '',
     otherActivity: '',
-    notes: 'Thanks for attending practice!',
+    notes: '',
   },
 
   // AlexanderK9
@@ -186,7 +186,7 @@ const ACTIVITY = {
   // Exar Kit
   5243: {
     communication: '',
-    flightActivity: 'Battlefront PvP, Star Conflict PvE',
+    flightActivity: '',
     otherActivity: '',
     notes: '',
   },
@@ -223,10 +223,7 @@ export default function Report({ activityData }) {
 	  
         <center><h4><span style={{ color: "#1F51FF" }}>Squadron Commander's Speech</span></h4></center>
 		
-		<p>Greetings pilots! As January draws to a close, pilots have been gearing up for the start of the Chalquilla Cup, our own Chalquilla Cup II team, Sintoxicated, has been busy preparing our starfighters and getting some last-minute practice in. We’ll be hoping to compete in Round 1 within the next week. </p>
-		<p>Congratulations go to CM Kazraran and GN Exar Kit this week for your well-earned Imperial Achievement Ribbons (IARs)! Thank you for the superb activity! </p>
-		<p>Our own LCM Kane Polybius has completed no less than six new chapters to his ongoing Black TIE Down fiction series. Please take some time to enjoy them on the #runons Discord channel. </p>
-		<p>There is currently a huge range of competitions to get involved with, please see the competitions section below for some of the ways you can get involved and participate with fleet activities. </p>
+		<p>_____TODO_____</p>
 
 
       </Intro>
@@ -235,15 +232,13 @@ export default function Report({ activityData }) {
 
                 <center><h3><span style={{ color: "#1F51FF" }}>Squadron News</span></h3></center>
         <ul>
-				<li>Practice session 1 for Sintoxicated, our Chalquilla Cup 2 team, went ahead last night and was a great success. Thanks to MAJ Wreckage, MAJ Robert Hogan, SA Kamjin Maverick Lap'lamiz for attending and apologies to LCM KEBLAOMEGA for narrowly missing you. The team saw some excellent progression over the course of the session and has primed us for our upcoming first round with The Funky Chickens (Firebird Squadron).</li><br></br>
-				<li>MSE (Monthly Squadron Evaluation) season us upon us once again, I’ll be completing evaluations within the next couple of days. So now is the perfect time to get those last-minute activities in! </li><br></br>
-				<li>Next week will mark the final week of our Sinfantry Training competition which will be held on Saturday the 5th of Feb at 9pm UTC.</li><br></br>
+				<li>_____TODO_____</li><br></br>
         </ul>
 		
                 <center><h3><span style={{ color: "#1F51FF" }}>Fleet News</span></h3></center>
         <ul>
-				<li>Episode 4 of CPT LegionX’s Airlock PodCast was aired. You can watch the recording of the PodCast <a href="https://www.twitch.tv/videos/1278217460"> here</a>. <br></br>Sin Squadron has been invited to this week’s PodCast, if you’re interested in attending the live stream, please contact me.</li><br></br>
-                <li><b>TIE Corps in Battle Season 4 Assignments for January:</b> 
+				<li>_____TODO_____</li><br></br>
+                <li><b>TIE Corps in Battle Season 4 Assignments for January_____TODO_____:</b> 
 					<br></br><br></br>
 					+ <a href="https://tc.emperorshammer.org/download.php?id=471&type=info"> TIE-TC 165: The Hunt for Zsinj </a>
 					<br></br><br></br>
@@ -268,6 +263,7 @@ export default function Report({ activityData }) {
 
       <Closing>
 	  
+		<p>_____TODO_____</p>
 		<p>My door is always open to all of you. If you need any help, guidance or just direction for some activities then please don’t hesitate to contact me on Discord of by email. If you’re online and gaming, drop in to one of our voice channels for visibility. You never know you might end up flying with some fellow Sinners or other members of the Corps.</p>
 		<p>For Sin! </p>
 		<p>CPT SkyShadow </p>
